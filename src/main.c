79187c2e01a233070d14885a94c403de58aa9961
Raja <rdas6313@gmail.com>
2025-05-08 15:49:27 +0000

    new changes

--- src/main.c
+++ src/main.c
@@ -1,6 +1,155 @@
+#include <stdio.h>
+#include<stdint.h>
+
+__attribute__((optimize("O0")))
+void test(){
+    int y = 10;
+    printf("Hello from Linux\n");
+}
+
+void USART1_IRQHandler(void){
+    //printf("Usart!");
+    uint32_t* ISPR = (uint32_t*)(0XE000E200);
+    *ISPR |= (1<<0);
+    asm volatile("mov r0,#1");
+    asm volatile("mov r1,#2");
+}
+
+void USART2_IRQHandler(void){
+    //printf("Usart!");
+    asm volatile("mov r0,#2");
+}
+void USART3_IRQHandler(void){
+    //printf("Usart!");
+    asm volatile("mov r0,#3");
+}
+
+void WWDG_IRQHandler(void){
+    asm volatile("mov r0,#4");
+}
+
+int add(int a,int b){
+    return a+b;
+}
+
+void setpriority(uint8_t irq_no,uint32_t priority){
+    uint32_t *IPR_BASE = (uint32_t*)0xE000E400;
+    uint8_t register_no = irq_no / 4;
+    uint8_t index = irq_no % 4;
+    uint8_t shift_value = index * 8;
+    uint32_t* final = (IPR_BASE + register_no);
+    *final |= (priority << shift_value);
+    
+}
+
+void HardFault_Handler(void){
+    while(1);
+}
+
+__attribute__((naked))
+void UsageFault_Handler(void){
+    asm volatile("MRS r0,MSP");
+    asm volatile("b UsageFault_Handler_C");
+}
+
+void UsageFault_Handler_C(uint32_t* stack){
+    
+    while(1);
+}
+
+__attribute__((naked))
+void Svc_handler(void){
+    asm volatile("MRS r0,MSP");
+    asm volatile("b Svc_handler_c");
+}
+
+void Svc_handler_c(uint32_t* stack_ptr){
+    uint8_t* pc = (uint8_t*)stack_ptr[6];
+    pc -= 2;
+    uint8_t irq = *pc;
+    uint8_t a,b;
+    switch(irq){
+        case 5:
+            a = stack_ptr[0];
+            b = stack_ptr[1];
+            stack_ptr[0] = a + b;
+            break;
+        default:
+            stack_ptr[0] = 0;
+    }
+
+}
+
+void main()
+{
+
+    
+    // int x = 2;
+    // x = x + 1;
+    // printf("X= %d  \n", x);
+    // my_label:
+    // printf("You are at my_label\n");
+
+    // void *addr = (void*)0x000002d9;
+
+    // asm volatile("mov r0,%0" ::"r"(addr));
+    // goto *addr;
+
+    
+    // void (*p)(void) = (void *)(0x000002a5);
+    // p();
+    // x = 10;
+    // uint8_t x = 0xff;
+    // uint8_t *q = &x;
+
+    // uint8_t *p = (uint8_t *)((uint32_t)0x22000000 + (32 * ((uint32_t)&x - (uint32_t)0x20000000)) + (7 * 4));
+
+    // *p = 0;
+
+    // asm volatile("MRS r1, PRIMASK");
+    // asm volatile("MRS r2, FAULTMASK");
+
+    // int data[] = {2,3};
+    // int c = add(data[0],data[1]);
+
+    
+    
+    // setpriority(0,0x00);
+    // setpriority(37,0x00);
+
+    // uint32_t* ISER = (uint32_t*)(0xE000E104);
+    // *ISER |= (1<<5);
+
+    // uint32_t* ISER0 = (uint32_t*)(0xE000E100);
+    // *ISER0 |= (1<<0);
+
+    // uint32_t* ISPR = (uint32_t*)(0XE000E204);
+    // *ISPR |= (1<<5);
+    
+    // uint32_t* config_register = (uint32_t*)0xE000ED14;
+    // *config_register |= (1<<4);
+    // uint32_t* SHCR = (uint32_t*)0xE000ED24;
+    // *SHCR |= (1<<18);
+    // int a = 5;
+    // int b = 0;
+    // int x = a/b;
+    if (1)
+    printf("%d\n", 123);
+    int result;
+    asm volatile("mov r0, #10");
+    asm volatile("mov r1, #5");
+    asm volatile("SVC #0x5");
+    asm volatile("str r0, %0":"=m"(result));
+   
+    printf("Semihost activated\n");
+    
+    printf("Result %d\n",result);
+    // fflush(stdout);
+    // printf("Hello\n");
+    // fflush(stdout);
+    char buffer[100];
+    sprintf(buffer, "Result = %d\n", result);
+    puts(buffer);
 
 
-void main(){
-    int x = 20;
-    x = x + 1;
 }
--- src/startup.c
+++ src/startup.c
@@ -2,14 +2,16 @@
 #define KB 1024
 #define RAM_SIZE (4 * KB)
 #define START_RAM 0x20000000
-#define END_RAM (START_RAM + RAM_SIZE)
+#define END_RAM (START_RAM + RAM_SIZE - 4)
 #define STACK_POINTER END_RAM
 
-void default_handler();
-void Reset_handler();
-//void Nmi_handler() __attribute__((weak,alias("default_handler")));
-void main(void);
-void __libc_init_array(void);
+// void default_handler();
+// void Reset_handler();
+// //void Nmi_handler() __attribute__((weak,alias("default_handler")));
+void main(void) __attribute__((weak,alias("default_handler")));
+void __libc_init_array(void) __attribute__((weak,alias("default_handler")));
+void initialise_monitor_handles();
+// void Usart_handler(void) __attribute__((weak,alias("default_handler")));
 
 extern uint32_t _etext;
 extern uint32_t _sdata;
@@ -18,17 +20,126 @@ extern uint32_t _sbss;
 extern uint32_t _ebss;
 extern uint32_t _ldata_;
 
-uint32_t vector_table[73] __attribute__((section(".isr_table"))) = {
-    STACK_POINTER,
-    (uint32_t)Reset_handler
-//    (uint32_t)Nmi_handler
+
+
+void Reset_Handler(void);
+void NMI_Handler(void) __attribute__((weak,alias("default_handler")));
+void HardFault_Handler(void) __attribute__((weak,alias("default_handler")));
+void UsageFault_Handler(void) __attribute__((weak,alias("default_handler")));
+void Default_Handler(void) __attribute__((weak,alias("default_handler")));
+void Svc_handler(void) __attribute__((weak,alias("default_handler")));
+
+void WWDG_IRQHandler(void) __attribute__((weak,alias("default_handler")));
+void PVD_IRQHandler(void) __attribute__((weak,alias("default_handler")));
+void TAMPER_IRQHandler(void) __attribute__((weak,alias("default_handler")));
+void RTC_IRQHandler(void) __attribute__((weak,alias("default_handler")));
+void FLASH_IRQHandler(void) __attribute__((weak,alias("default_handler")));
+void RCC_IRQHandler(void) __attribute__((weak,alias("default_handler")));
+void EXTI0_IRQHandler(void) __attribute__((weak,alias("default_handler")));
+void EXTI1_IRQHandler(void) __attribute__((weak,alias("default_handler")));
+void EXTI2_IRQHandler(void) __attribute__((weak,alias("default_handler")));
+void EXTI3_IRQHandler(void) __attribute__((weak,alias("default_handler")));
+void EXTI4_IRQHandler(void) __attribute__((weak,alias("default_handler")));
+void DMA1_Channel1_IRQHandler(void) __attribute__((weak,alias("default_handler")));
+void DMA1_Channel2_IRQHandler(void) __attribute__((weak,alias("default_handler")));
+void DMA1_Channel3_IRQHandler(void) __attribute__((weak,alias("default_handler")));
+void DMA1_Channel4_IRQHandler(void) __attribute__((weak,alias("default_handler")));
+void DMA1_Channel5_IRQHandler(void) __attribute__((weak,alias("default_handler")));
+void DMA1_Channel6_IRQHandler(void) __attribute__((weak,alias("default_handler")));
+void DMA1_Channel7_IRQHandler(void) __attribute__((weak,alias("default_handler")));
+void ADC1_IRQHandler(void) __attribute__((weak,alias("default_handler")));
+
+// 3 reserved slots (0x8C to 0x98)
+void TIM1_BRK_TIM15_IRQHandler(void) __attribute__((weak,alias("default_handler")));
+void TIM1_UP_TIM16_IRQHandler(void) __attribute__((weak,alias("default_handler")));
+void TIM1_TRG_COM_TIM17_IRQHandler(void) __attribute__((weak,alias("default_handler")));
+void TIM1_CC_IRQHandler(void) __attribute__((weak,alias("default_handler")));
+void TIM2_IRQHandler(void) __attribute__((weak,alias("default_handler")));
+void TIM3_IRQHandler(void) __attribute__((weak,alias("default_handler")));
+void TIM4_IRQHandler(void) __attribute__((weak,alias("default_handler")));
+void I2C1_EV_IRQHandler(void) __attribute__((weak,alias("default_handler")));
+void I2C1_ER_IRQHandler(void) __attribute__((weak,alias("default_handler")));
+void I2C2_EV_IRQHandler(void) __attribute__((weak,alias("default_handler")));
+void I2C2_ER_IRQHandler(void) __attribute__((weak,alias("default_handler")));
+void USART1_IRQHandler(void) __attribute__((weak,alias("default_handler")));
+void USART2_IRQHandler(void) __attribute__((weak,alias("default_handler")));
+void USART3_IRQHandler(void) __attribute__((weak,alias("default_handler")));
+
+
+ uint32_t vector_table[] __attribute__((section(".isr_table"))) = {
+    STACK_POINTER,                   // 0: Initial SP
+    (uint32_t)Reset_Handler,              // 1: Reset
+    (uint32_t)NMI_Handler,                // 2: NMI
+    (uint32_t)HardFault_Handler,          // 3: Hard Fault
+    (uint32_t)Default_Handler,            // 4: MemManage (unused)
+    (uint32_t)Default_Handler,            // 5: BusFault (unused)
+    (uint32_t)UsageFault_Handler,            // 6: UsageFault (unused)
+    0, 0, 0, 0,                            // 7-10: Reserved
+    (uint32_t)Svc_handler,            // 11: SVCall
+    (uint32_t)Default_Handler,            // 12: Debug Monitor
+    0,                                    // 13: Reserved
+    (uint32_t)Default_Handler,            // 14: PendSV
+    (uint32_t)Default_Handler,            // 15: SysTick
+
+    // External Interrupts (IRQs)
+    (uint32_t)WWDG_IRQHandler,            // 0
+    (uint32_t)PVD_IRQHandler,             // 1
+    (uint32_t)TAMPER_IRQHandler,          // 2
+    (uint32_t)RTC_IRQHandler,             // 3
+    (uint32_t)FLASH_IRQHandler,           // 4
+    (uint32_t)RCC_IRQHandler,             // 5
+    (uint32_t)EXTI0_IRQHandler,           // 6
+    (uint32_t)EXTI1_IRQHandler,           // 7
+    (uint32_t)EXTI2_IRQHandler,           // 8
+    (uint32_t)EXTI3_IRQHandler,           // 9
+    (uint32_t)EXTI4_IRQHandler,           // 10
+    (uint32_t)DMA1_Channel1_IRQHandler,   // 11
+    (uint32_t)DMA1_Channel2_IRQHandler,   // 12
+    (uint32_t)DMA1_Channel3_IRQHandler,   // 13
+    (uint32_t)DMA1_Channel4_IRQHandler,   // 14
+    (uint32_t)DMA1_Channel5_IRQHandler,   // 15
+    (uint32_t)DMA1_Channel6_IRQHandler,   // 16
+    (uint32_t)DMA1_Channel7_IRQHandler,   // 17
+    (uint32_t)ADC1_IRQHandler,            // 18
+    0,                                    // 19: Reserved (0x8C)
+    0,                                    // 20: Reserved (0x90)
+    0,                                    // 21: Reserved (0x94)
+    (uint32_t)TIM1_BRK_TIM15_IRQHandler,  // 22
+    (uint32_t)TIM1_UP_TIM16_IRQHandler,   // 23
+    (uint32_t)TIM1_TRG_COM_TIM17_IRQHandler, // 24
+    (uint32_t)TIM1_CC_IRQHandler,         // 25
+    (uint32_t)TIM2_IRQHandler,            // 26
+    (uint32_t)TIM3_IRQHandler,            // 27
+    (uint32_t)TIM4_IRQHandler,            // 28
+    (uint32_t)I2C1_EV_IRQHandler,         // 29
+    (uint32_t)I2C1_ER_IRQHandler,         // 30
+    (uint32_t)I2C2_EV_IRQHandler,         // 31
+    (uint32_t)I2C2_ER_IRQHandler,         // 32
+    (uint32_t)USART1_IRQHandler,          // 33
+    (uint32_t)USART2_IRQHandler,          // 34
+    (uint32_t)USART3_IRQHandler,           // 35
+    (uint32_t)USART3_IRQHandler,          // 36
+    (uint32_t)USART1_IRQHandler,          // 37
+    (uint32_t)USART2_IRQHandler           // 38
 };
 
-// void default_handler(){
-//     // default_handler
-// }
 
-void Reset_handler(){
+
+// uint32_t vector_table[73] __attribute__((section(".isr_table"))) = {
+//     STACK_POINTER,
+//     (uint32_t)Reset_handler
+// //    (uint32_t)Nmi_handler
+// };
+
+
+
+
+void default_handler(){
+    // default_handler
+    asm volatile("mov r0, #5");
+}
+
+void Reset_Handler(){
     //reset_handler
     /*Copying data section from flash to SRAM */
     uint32_t size = (uint32_t)&_edata - (uint32_t)&_sdata;
@@ -44,9 +155,12 @@ void Reset_handler(){
         *ptr_ram++ = 0;
     
     //initializing Newlib c library
-    __libc_init_array();
+   __libc_init_array();
+   initialise_monitor_handles();
+    
     //calling main
     main();
+    //while(1);
 
 }
 
